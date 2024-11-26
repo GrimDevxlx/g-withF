a5abefa036e0dafee04f5fdeb5f49413dc0b1b4d
GrimDevxlx <slj1999x@gmail.com>
2024-11-26 21:07:38 +0800

    ListApps

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-import ChannelListContainer from './components/ChannelListContainer';
-import ChannelContainer from './components/ChannelContainer';
+import { ChannelListContainer, ChannelContainer} from './components';
+import './App.css';
 
 const apiKey = 'ucffy8yaqj2w'; //API KEY STREAM.IO 
 const client = StreamChat.getInstance(apiKey);
+
+
 
 const App = () => {
   return (
