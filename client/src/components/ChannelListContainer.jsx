d0806d836deddb893c3c59814a2abb5d4887c2bc
GrimDevxlx <slj1999x@gmail.com>
2024-11-27 19:44:52 +0800

    ChannelList

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,22 +3,30 @@ import { ChannelList, useChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 import GamingWFIcon from '../assets/GamingWF.png'
+import LogoutIcon from '../assets/logout.png'
 
-const SideBar = () => {
+const SideBar = () => (
   <div className="channel-list__sidebar">
       <div className="channel-list__sidebar__icon1">
         <div className="icon1__inner">
           <img src= {GamingWFIcon} alt="GamingWF" width="30" />
         </div>
       </div>
+      <div className="channel-list__sidebar__icon2">
+        <div className="icon1__inner">
+          <img src= {LogoutIcon} alt="Logout" width="30" />
+        </div>
+      </div>
   </div>
-}
+);
+
+
 
 const ChannelListContainer = () => {
   return (
-    <div>
-      ChannelListContainer
-    </div>
+    <>
+      <SideBar />
+    </>
   );
 }
 
