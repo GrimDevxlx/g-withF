2eb6c730dbde160351a509313ac14c87a74b0b5d
GrimDevxlx <slj1999x@gmail.com>
2024-11-28 17:57:10 +0800

    autToken1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,19 @@ import React from 'react'
 import { StreamChat } from 'stream-chat';
 import { Chat } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
-import { ChannelListContainer, ChannelContainer} from './components';
+import { ChannelListContainer, ChannelContainer, Auth } from './components';
 import './App.css';
 
 const apiKey = 'ucffy8yaqj2w'; //API KEY STREAM.IO 
 const client = StreamChat.getInstance(apiKey);
 
-
+const authToken = false;
 
 const App = () => {
+
+    if(authToken) return <Auth />
+
+    
   return (
     <div className="app_wrapper">
       <Chat client = {client} theme="team light">
