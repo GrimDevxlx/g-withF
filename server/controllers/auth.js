e04a317935ae9fedf85999381ee555cf155af844
GrimDevxlx <slj1999x@gmail.com>
2024-12-01 13:31:06 +0800

    async-await!
    TOKEN

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -11,11 +11,11 @@ const crypto = require('crypto');
 const signup = async (req, res) => {
   try {
       const { fullName, username, password, phoneNumber } = req.body;
-      const userId = crypto.randomBytes(16).toString('hex');
-      const serverClient = connect(api_key, api_secret, app_id);
+        const userId = crypto.randomBytes(16).toString('hex');
+          const serverClient = connect(api_key, api_secret, app_id);
 
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const token = serverClient.createUserToken(userId);
+                const hashedPassword = await bcrypt.hash(password, 10);
+                    const token = serverClient.createUserToken(userId);
           res.status(200).json({ token, fullName, username, userId, hashedPassword, phoneNumber});
 
   } catch (error) {
@@ -26,10 +26,24 @@ const signup = async (req, res) => {
 };
 
 
-const login = (req, res) => {
+const login = async (req, res) => {
   try {
-    
-  } catch (error) {
+      const { username, password } = req.body;
+          const serverClient = connect(api_key, api_secret, app_id);
+            const client = StreamChat.getInstance(api_key, api_secret);
+                const { users } = await client.queryUsers({ name: username });
+
+    if(!users.length) return res.status(400).json({ message: 'User not found'});
+      const success = await bcrypt.compare(password, users[0].hashedPassword);
+        const token = serverClient.createUserToken(users[0].id);
+
+        if(success) {
+          res.status(200).json({ token, fullName: users[0].fullName, username, userId: users[0].id});
+        } else {
+          res.status(500).json({ message: 'Wrong Password fool!'});
+        }
+
+  } catch (error) {ads
     console.log(error);
     res.status(500).json({ message: error });
   }
